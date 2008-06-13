f4c74618a5be9f84814cb9cd95c46936a1181df0
Sanath Kumar <isiri@isiri-laptop.(none)>
2008-06-13 20:45:52 +0530

    modified:   spec/models/trike_tags_spec.rb

--- spec/models/trike_tags_spec.rb
+++ spec/models/trike_tags_spec.rb
@@ -14,7 +14,7 @@ describe "Trike Tags" do
     end
 
     it "<r:site_area /> should return the top level parent page slug" do
-        page(:home).should render("<r:site_area />").as('homepage')  #doubt, should not it be as '' ?
+        page(:home).should render("<r:site_area />").as('homepage')  #doubt, shouldn't it be as '' ?
         page(:parent).should render("<r:site_area />").as('parent')  
         page(:child).should render("<r:site_area />").as('parent')
         page(:grandchild).should render("<r:site_area />").as('parent')
@@ -63,14 +63,15 @@ describe "Trike Tags" do
     it "<r:current_if_same_site_subarea /> returns 'current' if the local page context is in the same site_subarea as the global page context." do
         page(:home).should render("<r:find url='/parent/child'><r:current_if_same_site_subarea /></r:find>").as('') 
 
-        page(:parent).should render("<r:find url='/parent'><r:current_if_same_site_subarea /></r:find>").as('current') #d, should not it be as '' ?
+        page(:parent).should render("<r:find url='/parent'><r:current_if_same_site_subarea /></r:find>").as('current') #doubt, shouldn't it be as '' ?
         page(:parent).should render("<r:find url='/parent/child'><r:current_if_same_site_subarea /></r:find>").as('')
         page(:parent).should render("<r:find url='/parent/child/grandchild'><r:current_if_same_site_subarea /></r:find>").as('')
-=begin
-        page(:parent).should render("<r:find url='/parent2'><r:current_if_same_site_subarea /></r:find>").as('')
+
+    #   the below test fails
+    #   page(:parent).should render("<r:find url='/parent2'><r:current_if_same_site_subarea /></r:find>").as('')
         page(:parent).should render("<r:find url='/parent2/child2'><r:current_if_same_site_subarea /></r:find>").as('')
         page(:parent).should render("<r:find url='/parent2/child2/grandchild2'><r:current_if_same_site_subarea /></r:find>").as('')
-=end
+
         
         page(:child).should render("<r:find url='/parent'><r:current_if_same_site_subarea /></r:find>").as('')
         page(:child).should render("<r:find url='/parent/child'><r:current_if_same_site_subarea /></r:find>").as('current')
