64f3c26ecbf4a5e7b15cf1d086be4f3ed6b8d422
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2010-09-10 16:12:25 +1000

    add <r:escape:csv> tag

--- app/models/escape_tags.rb
+++ app/models/escape_tags.rb
@@ -15,4 +15,18 @@ module EscapeTags
   tag "escape:uri" do |tag|
     URI.escape tag.expand
   end
+
+  desc %{
+    CSV escapes its contents.
+    
+    *Usage:*
+    <pre><code><r:escape:csv>some, "content" that, might<br />
+otherwise, cause trouble</r:escape:csv></code></pre>
+    <pre><code> => some, """content"" that", "might<br />
+otherwise", cause trouble</code></pre>
+  }
+  tag "escape:csv" do |tag|
+    "\"#{tag.expand.gsub(/"/,'""')}\""
+  end
+
 end
--- spec/models/escape_tags_spec.rb
+++ spec/models/escape_tags_spec.rb
@@ -17,5 +17,18 @@ describe ": escape tags :" do
       @page.should render("<r:escape:uri>this thing</r:escape:uri>").as("this%20thing")
     end
   end
+
+  describe "<r:escape:csv />" do
+    fixture = {
+      'content' => '"content"',
+      'with "quotes"' => '"with ""quotes"""',
+      'with, comma' => '"with, comma"',
+    }
+    fixture.each do |input, output|
+      it "should properly escape #{input} to #{output}" do
+        @page.should render("<r:escape:csv>#{input}</r:escape:csv>").as(output)
+      end
+    end
+  end
 end
 
